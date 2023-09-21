7b50016f8e0c220da0e18fc772a47acf894ce8c8
amayorga7904 <alexmayorga20@gmail.com>
2023-09-21 00:31:15 -0700

    most pseudocode completedand html is setup

---
+++ node.js
@@ -0,0 +1,165 @@
+
+
+
+
+
+
+//DEFINE CONSTANTS
+// Initial player's score
+
+// Score needed to win
+
+// Initial count of lives set to 3
+
+// ID of the game board element
+
+// ID of the start button element
+
+// ID of the easy button element
+
+// ID of the normal button element
+
+// ID of the hard button element
+
+// ID of the hit button element
+
+// ID of the score display element
+
+// ID of the lives display element
+
+
+
+
+
+
+//DEFINE STATE VARIABLES
+// Player's score
+
+// Count of lives
+
+// Word choice count
+
+// Background images
+
+
+
+//CACHE ELEMENTS
+// Start button element
+
+// Game board element
+
+// Score display element
+
+// Lives display element
+
+// Wrong Choices display element
+
+
+
+
+
+//ADD EVENT LISTENERS
+// Event listener for the start button
+
+// Event listener for the Easy button
+
+// Event listener for the Normal button
+
+// Event listener for the Hard button
+
+// Event listener for the Hint button
+
+// Event listener for the key presses
+
+// Event listener for the gameboard
+
+
+
+
+
+//INVOKE INIT FUNCTIONS
+ // Reset the score to the initial value
+ 
+ // Reset lives to the initial value
+
+ // Disable the start button to prevent multiple game starts
+
+ // Hide the start button
+
+ // Clear the game board
+
+ // Render the game state onto the DOM
+ 
+ 
+
+ 
+
+ //INVOKE RENDER FUNCTIONS 
+ // Set the score display to the current score
+
+ // Set the lives display to the current lives
+
+ // Set the wrong choices display to the current wrong choices
+
+
+
+
+//WAIT FOR TRIGGERED EVENTS
+
+
+
+
+//REPLAY/RESTART
+// Update the score display
+
+// Show the start button with the text "Replay"
+
+// Show the button
+
+// Re-enable the start button
+
+// Clear the game board
+
+// Clear the lives display
+
+
+
+
+
+//UPDATE STATE VARIABLES
+// Set the score display to the current score
+
+ // Set the lives display to the current lives
+
+ // Set the wrong choices display to the current wrong choices
+
+
+
+
+
+//INVOKE RENDER FUNCTION
+// Set the score display to the current score
+
+ // Set the lives display to the current lives
+
+ // Set the wrong choices display to the current wrong choices
+
+
+
+// IGNORE!!
+//  const START_BUTTON = 'start-button'
+//  const startButton = document.getElementById(START_BUTTON)
+// const hideButton = document.getElementById('hint-button')
+
+//  function changeColor() {
+//     document.getElementById('body').style.backgroundImage= "url(https://media.istockphoto.com/id/1434212178/photo/middle-eastern-lady-using-laptop-working-online-sitting-in-office.jpg?s=1024x1024&w=is&k=20&c=H640-Mts2rHSHLTkCd04WFd_VhcHMwX8kAGVXW4ddJY=)"
+//     startButton.innerHTML = 'REPLAY'
+//  }
+
+//  function color() {
+//     document.getElementById('body').style.backgroundImage= "url(https://media.istockphoto.com/id/1589244726/photo/woman-remote-work-and-typing-at-laptop-in-home-for-digital-planning-online-research-and.jpg?s=612x612&w=is&k=20&c=QWSS2SkXegIVI6GzLnXQ8IM4GHgsfQ-DoKyg9hOq2qw=)"
+//     hideButton.innerHTML = 'DUMBASS'
+//  }
+//  hideButton.addEventListener('click', color)
+//  startButton.addEventListener('click', changeColor)
+
